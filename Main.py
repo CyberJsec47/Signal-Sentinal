f1461c014534b5dae87cc82310f07bfca3a0d1eb
Cyber-Waves <149782160+CyberJsec47@users.noreply.github.com>
2025-01-27 18:54:53 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 from calculations import *
 from pyfiglet import Figlet
 from colorama import Fore
-from rawIQ import *
+from SigCapture import *
+from Functions import *
 
 
 def main():
@@ -17,21 +18,27 @@ def main():
         print(Fore.BLUE + "-"*25)
         print(Fore.BLUE + "| Capture a signal   [1] |")
         print(Fore.BLUE + "-"*25)
-        print(Fore.BLUE + "| Add signal to CSV  [2] |")
+        print(Fore.BLUE + "| View signal data   [2] |")
         print(Fore.BLUE + "-"*25)
         print(Fore.BLUE + "| Analyse Signal     [3] |")
         print(Fore.BLUE + "-"*25)
         print(Fore.BLUE + "| Exit               [4] |")
         print(Fore.BLUE + "-"*25)
-        option = int(input(Fore.BLUE + "| Choose an option:"))
+        option = int(input(Fore.BLUE + "| Choose an option: "))
         if option == 1:
-            print(Fore.BLUE + "Capturing signal...")
-            capture_signal(top_block_cls=rawIQ, options=None)
-
-
-
-
-
-
-
-main()
+            print(Fore.BLUE + "Choose a frequency and capture time")
+            freq_hz = freq_select()
+            seconds = int(input(Fore.BLUE + "Time in Seconds: "))
+            get_signal(seconds, freq_hz)
+            main()
+        elif option == 2:
+            capture_menu()
+            break
+        elif option == 3:
+            print(Fore.RED + "Placeholder")
+            main()
+        elif option == 4:
+            print(Fore.RED + f.renderText("Exiting"))
+            break
+        else:
+            print(Fore.RED + "Select an option")
