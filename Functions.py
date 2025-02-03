881a5efba1cc9dcfa2ee74d80654d5ed6f3c4f08
CyberJsec47 <cyberjsec47@gmail.com>
2025-02-03 18:12:27 +0000

    edit

--- Functions.py
+++ Functions.py
@@ -4,7 +4,7 @@ from rtlsdr import RtlSdr
 import time
 from colorama import Fore
 import csv
-
+from Calculations import *
 
 def freq_select():
     frequency = float(input(Fore.GREEN + "Choose a frequency to use (MHz): "))
@@ -37,7 +37,7 @@ def get_signal(seconds, frequency):
                 break
 
         captured_samples_np = np.array(captured_samples, dtype=np.complex64)
-        output_path = 'iq_samples.dat'
+        output_path = 'rolling_test.dat'
         captured_samples_np.tofile(output_path)
 
         print(Fore.GREEN + f"Capture complete. collected {len(captured_samples)} samples\n Saving to {output_path}")
@@ -67,3 +67,38 @@ def view_CSV(file_path, num_rows):
  
 
             
+
+
+def rolling_window(seconds, frequency):
+    file = 'rolling_test.dat'
+    iq_data = np.fromfile(file, dtype=np.complex64)
+    fs = 1_000_000
+    sdr = RtlSdr()
+
+    try:
+        freq = sdr.center_freq = frequency
+        fs = sdr.fs = 1e6
+        sdr.gain = 'auto'
+
+        total_samples = int(seconds * fs)
+        chunk_size = 256 * 1024
+        captured_samples = []
+        start_time = time.time()
+
+        while len(captured_samples) < total_samples:
+            sampled_needed = total_samples - len(captured_samples)
+            chunk = sdr.read_samples(min(chunk_size, sampled_needed))
+            captured_samples.extend(chunk)
+
+            if time.time() - start_time >= seconds:
+                break
+
+        captured_samples_np = np.array(captured_samples, dtype=np.complex64)
+        output_path = 'rolling_test.dat'
+        captured_samples_np.tofile(output_path)
+        feature_extraction(iq_data, fs)
+    finally:
+        sdr.close()
+
+
+
